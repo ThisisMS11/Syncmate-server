4806f17dcf0b2919eb7eb5ed5db191f9dc9e339a
Mohit Saini <mailtomohit2002@gmail.com>
2025-04-20 13:03:50 +0530

    Initial Setup

---
+++ src/main/java/com/SyncMate/SyncMate/SyncMateApplication.java
@@ -0,0 +1,13 @@
+package com.SyncMate.SyncMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SyncMate/SyncMate/SyncMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SyncMate.SyncMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
